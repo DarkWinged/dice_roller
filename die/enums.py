154c73c97c7617dc97b3a5a903f842a09b7ba938
James Rogers <James.Levi.Rogers@gmail.com>
2022-03-06 18:25:06 -0500

    Revert "Initial Commit"
    This reverts commit a69af0669b92745166655aa87b38f1f0811f47ec.

--- die/enums.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-
-class Advantage(enum.IntEnum):
-    adv = 1
-    no = 0
-    dis = -1
--- main.py
+++
@@ -1,9 +0,0 @@
-def game_init(): pass
-
-
-def game_loop(): pass
-
-
-if __name__ == '__main__':
-    game_init()
-    game_loop()
