e3736d893d74e3eb9d110aa6ab71d31ed57dc8c5
James Rogers <James.Levi.Rogers@gmail.com>
2022-03-06 21:39:06 -0500

    room_functions  new:    Room.flood_fill    Room.new    Room.add_tiles    Room.add_token  fixed:    tile position being stored incorrectly

--- room.py
+++ room.py
@@ -49,32 +49,46 @@ def transfer_token(token_name: str, origin: Tile, destination: Tile):
     destination.add_token(token_to_move)
 
 
+def get_neighbors(point: tuple[int, int]) -> tuple[tuple[int, int], list[tuple[int, int]]]:
+    return point, [(point[0]+1, point[1]), (point[0], point[1]+1), (point[0]-1, point[1]), (point[0], point[1]-1)]
+
+
 class Room:
-    def __init__(self, seed: int, size: tuple[int, int], tile_set: list[str]):
-        self._tiles = {}
+    def __init__(self, seed: int, size: tuple[int, int], tile_set: list[str], tile_map: dict[tuple[int, int], Tile] = None):
+        if tile_map is None:
+            self._tiles = {}
+            for x in range(size[0]):
+                for y in range(size[1]):
+                    self._tiles[(x, y)] = Tile((x, y), {})
+        else:
+            self._tiles = tile_map
         self._tokens: dict[str, MapToken] = {}
         self._seed = seed
         self._size = size
         self._tile_set: dict[int, str] = {}
         for index, icon in enumerate(tile_set):
             self._tile_set[index - 1] = icon
-        for x in range(size[0]):
-            for y in range(size[1]):
-                self._tiles[(x, y)] = Tile((x, y), {})
+
+    @classmethod
+    def new(cls, seed: int, tile_set: dict[str, dict[str, any]], tile_map: list[list[str]]):
+        length_y, length_x = len(tile_map), max([len(column) for column in tile_map])
+        size = (length_x, length_y)
+        tiles = {}
+        for y in range(len(tile_map)):
+            for x in range(len(tile_map[0])):
+                tiles[(x, y)] = Tile((x, y), tile_set[tile_map[y][x]])
+        new_room = Room(seed, size, tile_set.keys(), tiles)
+        return new_room
 
     @property
     def tokens(self):
         return self._tokens
 
-    @property
-    def tiles(self):
-        return self._tiles
-
     def __str__(self):
         string = ''
-        for x in range(self._size[0]):
-            for y in range(self._size[1]):
-                if self._tiles[(x, y)].icon in self._tile_set:
+        for y in range(self._size[1]):
+            for x in range(self._size[0]):
+                if (x, y) in self._tiles and self._tiles[(x, y)].icon in self._tile_set:
                     tile_icon = self._tile_set[self._tiles[(x, y)].icon]
                 else:
                     tile_icon = self._tile_set[-1]
@@ -82,9 +96,16 @@ class Room:
             string = f'{string}\n'
         return string
 
+    def add_tile(self, position: tuple[int, int], tile_type: dict[str, any] = None):
+        self._tiles[position] = Tile(position, tile_type)
+
+    @property
+    def tiles(self):
+        return self._tiles
+
     def add_token(self, new_token: MapToken):
         if new_token.name in self._tokens.keys() and new_token is not self._tokens[new_token.name]:
-            raise NameError(f'Name({new_token.name}: {new_token}) already exists in Room._tokens['
+            raise NameError(f'Name: {new_token.name, new_token} already exists in Room._tokens['
                             f'{self._tokens[new_token.name]}]')
         else:
             self._tokens[new_token.name] = new_token
@@ -104,6 +125,27 @@ class Room:
             return False
         else:
             return True
+
+    def flood_fill(self, start: tuple[int, int], movement_points: int) -> list[tuple[int, int]]:
+        fill = {}
+        neighbors = {}
+        if start in self._tiles:
+            fill[start] = movement_points
+            point, adjacent = get_neighbors(start)
+            neighbors[point] = adjacent
+            while len(neighbors) != 0:
+                points = []
+                for point in neighbors.keys():
+                    points.append(point)
+                for point in points:
+                    adjacent = neighbors.pop(point)
+                    for neighbor in adjacent:
+                        if neighbor in self._tiles and self._tiles[neighbor].passable is True and\
+                                fill[point] - self._tiles[neighbor].movement_cost >= 0:
+                            fill[neighbor] = fill[point] - self._tiles[neighbor].movement_cost
+                            new_point, adjacent = get_neighbors(neighbor)
+                            neighbors[new_point] = adjacent
+        return fill
 
     def get_tokens_in_range(self):
         pass
--- tests/token_unit_test.py
+++ tests/token_unit_test.py
@@ -1,4 +1,4 @@
-import map_token
+from map_token import MapToken
 import room
 from room import Room
 
@@ -29,10 +29,99 @@ def test_render_room():
     assert f'{box}' == test_string
 
 
+def test_add_tiles():
+    tile_set = ['E', 'F', 'W']
+    size = (5, 5)
+    test_room = Room(1234, size, tile_set)
+    test_string = 'FFFFF\n' \
+                  'FFFFF\n' \
+                  'FFWWW\n' \
+                  'FFWFF\n' \
+                  'FFFFF\n'
+
+    test_room.add_tile((4, 2), {'icon': 1, 'passable': False})
+    test_room.add_tile((3, 2), {'icon': 1, 'passable': False})
+    test_room.add_tile((2, 2), {'icon': 1, 'passable': False})
+    test_room.add_tile((2, 3), {'icon': 1, 'passable': False})
+
+    assert f'{test_room}' == test_string
+
+
+def test_flood_fill():
+    tile_set = {
+                'E': {'icon': -1},
+                'F': {'icon': 0},
+                'W': {'icon': 1, 'passable': False},
+                'R': {'icon': 2, 'movement_cost': 2}
+                }
+    tile_map = [
+                ['W', 'W', 'W', 'F', 'W', 'W', 'W'],
+                ['W', 'F', 'W', 'F', 'R', 'F', 'W'],
+                ['W', 'F', 'W', 'F', 'R', 'F', 'W'],
+                ['W', 'F', 'W', 'F', 'R', 'F', 'W'],
+                ['W', 'F', 'F', 'F', 'F', 'F', 'W'],
+                ['W', 'F', 'F', 'F', 'F', 'F', 'W'],
+                ['W', 'W', 'W', 'W', 'W', 'W', 'W']
+               ]
+    test_room = Room.new(5678, tile_set, tile_map)
+    fill_start = (3, 0)
+    fill_range = 4
+    result_expected = [fill_start, (3, 1), (3, 2), (3, 3), (3, 4), (4, 1), (4, 2), (5, 1)]
+
+    fill = test_room.flood_fill(fill_start, fill_range)
+
+    for point in result_expected:
+        assert point in fill.keys()
+
+
+def test_new_map():
+    tile_set = {'E': {'icon': -1}, 'F': {'icon': 0}, 'W': {'icon': 1, 'passable': False}}
+    tile_map = [
+                ['W', 'W', 'F', 'W', 'W'],
+                ['F', 'F', 'F', 'F', 'W'],
+                ['W', 'W', 'F', 'F', 'F'],
+                ['E', 'W', 'F', 'F', 'W'],
+                ['E', 'W', 'F', 'W', 'W']
+               ]
+    result = ''
+
+    test_room = Room.new(1234, tile_set, tile_map)
+
+    for y, column in enumerate(tile_map):
+        for x, tile in enumerate(column):
+            result = f'{result}{tile}'
+        result = f'{result}\n'
+
+    assert f'{test_room}' == result
+    for y, column in enumerate(tile_map):
+        for x, tile in enumerate(column):
+            print((x, y), test_room._tile_set[test_room.tiles[(x, y)].icon], tile)
+            assert test_room._tile_set[test_room.tiles[(x, y)].icon] == tile
+
+
+def test_add_token():
+    bob_name = 'bob'
+    bob_start = (0, 0)
+    bob_token = MapToken(bob_name, bob_start)
+    dan_name = 'dan'
+    dan_start = (5, 5)
+    dan_token = MapToken(dan_name, dan_start)
+    tile_set = ['E', 'F', 'W']
+    size = (5, 5)
+    test_map = Room(5678, size, tile_set)
+
+    test_map.add_token(bob_token)
+    test_map.add_token(bob_token)
+    test_map.add_token(dan_token)
+
+    assert bob_token in test_map.tokens.values()
+    assert dan_token in test_map.tokens.values()
+
+
 def test_move_token():
     token_name = 'fred'
     initial_position = (1, 1)
-    test_token = map_token.MapToken(token_name, initial_position)
+    test_token = MapToken(token_name, initial_position)
     tile_set = ['E', 'F', 'W']
     size = (5, 5)
     test_map = Room(5678, size, tile_set)
@@ -43,3 +132,7 @@ def test_move_token():
 
     assert test_map.tokens[token_name].position[0] == initial_position[0] + movement[0]
     assert test_map.tokens[token_name].position[1] == initial_position[1] + movement[1]
+
+
+def test_path_finding():
+    pass
